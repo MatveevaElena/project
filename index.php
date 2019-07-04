89f8f148fe3eaa173d7a9972eb9ec3399ec3a6c8
MatveevaElena <52277905+MatveevaElena@users.noreply.github.com>
2019-07-04 12:52:30 +0400

    first work with Bootstrap
    drop-down list does not work;
    the server does not process the entered information in any way.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-
+<!-- 
 <link rel="stylesheet" type="text/css" href="/../css/main.css">  
 <form action = "action.php" method="post">    
     <label for="Surname">Фамилия:</label>
@@ -17,5 +17,47 @@
             <option value="Италия"> Италия</option>  
         </select>
     <input type="submit" class="cursor" value="Отправить">
+</form>
+ -->
+ <!doctype html>
+
+
+ 
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<div class="p-3 mb-2  bg-dark text-white">
+<form>
+<div class="form-group">
+    <label for="inputSurname">Фамилия:</label>
+    <input type="text"  class="form-control bg-warning " id="inputSurname" >
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputName">Имя:</label>
+      <input type="text" class="form-control bg-warning" id="inputName" >
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPatronymic">Отчество:</label>
+      <input type="text" class="form-control bg-warning" id="inputPatronymic" >
+    </div>
+  </div>
+  <div class="form-group col-md-6">
+    <label for="inputBirthdate">Дата рождения:</label>
+    <input type="date" class="form-control bg-warning">
+  </div>
+    <div class="form-row">
+    <div class="dropdown">
+    <label for="inputCountry">Страна:</label>
+    <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Россия
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Россия</a>
+    <a class="dropdown-item" href="#">Испания</a>
+    <a class="dropdown-item" href="#">Мексика</a>
+  </div>
+</div>
+</div>
+</div>
+  <button type="submit" class="btn btn-warning btn-lg btn-block">Отправить</button>
 </form>
 
